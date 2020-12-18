c0123d47ef6622a5fe645fce2d783e76ca44e15e
vicboma1 <victorbolinchesmarin@gmail.com>
2020-12-18 01:18:55 +0100

    more tests

--- test/suite.test.js
+++ test/suite.test.js
@@ -5,7 +5,7 @@ const transistores = require('./test01.js');
 		var result = transistores(null);
 		expect(result).toBe(expected);
 	});
-	
+
 	test('transistores([])', function () {
 		var expected = 'Deben estar presentes al menos dos colores';
 		var result = transistores([]);
@@ -42,3 +42,100 @@ const transistores = require('./test01.js');
 		expect(result).toBe(expected);
 	});
 
+	test('transistores([Marrón, Rojo])', function () {
+		var expected = 12 ;
+		var result = transistores(['Marrón','Rojo']);
+		expect(result).toBe(expected);
+	});
+
+	test('transistores([Marrón, Naranja])', function () {
+		var expected = 13 ;
+		var result = transistores(['Marrón','Naranja']);
+		expect(result).toBe(expected);
+	});
+
+	test('transistores([Marrón, Amarillo])', function () {
+		var expected = 14 ;
+		var result = transistores(['Marrón','Amarillo']);
+		expect(result).toBe(expected);
+	});
+
+	test('transistores([Marrón, Verde])', function () {
+		var expected = 15 ;
+		var result = transistores(['Marrón','Verde']);
+		expect(result).toBe(expected);
+	});
+
+	test('transistores([Marrón, Azul])', function () {
+		var expected = 16 ;
+		var result = transistores(['Marrón','Azul']);
+		expect(result).toBe(expected);
+	});
+
+	test('transistores([Marrón, Violeta])', function () {
+		var expected = 17 ;
+		var result = transistores(['Marrón','Violeta']);
+		expect(result).toBe(expected);
+	});
+
+	test('transistores([Marrón, Blanco])', function () {
+		var expected = 19 ;
+		var result = transistores(['Marrón','Blanco']);
+		expect(result).toBe(expected);
+	});
+
+	test('transistores([Marrón, Gris])', function () {
+		var expected = 18 ;
+		var result = transistores(['Marrón','Gris']);
+		expect(result).toBe(expected);
+	});
+
+	
+	test('transistores([Marrón, Negro, Blanco])', function () {
+		var expected = 109 ;
+		var result = transistores(['Marrón','Negro','Blanco']);
+		expect(result).toBe(expected);
+	});
+
+	test('transistores([Rojo, Negro, Blanco])', function () {
+		var expected = 209 ;
+		var result = transistores(['Rojo','Negro','Blanco']);
+		expect(result).toBe(expected);
+	});
+
+	test('transistores([Rojo, Negro, Amarillo])', function () {
+		var expected = 204 ;
+		var result = transistores(['Rojo','Negro','Amarillo']);
+		expect(result).toBe(expected);
+	});
+
+	test('transistores([Verde, Azul, Violeta])', function () {
+		var expected = 567 ;
+		var result = transistores(['Verde','Azul','Violeta']);
+		expect(result).toBe(expected);
+	});
+
+	test('transistores([Verde, Azul, Violeta, Naranja])', function () {
+		var expected = 5673 ;
+		var result = transistores(['Verde','Azul','Violeta','Naranja']);
+		expect(result).toBe(expected);
+	});
+
+	test('transistores([Verde, Azul, Violeta, Naranja, Negro])', function () {
+		var expected = 56730 ;
+		var result = transistores(['Verde','Azul','Violeta','Naranja','Negro']);
+		expect(result).toBe(expected);
+	});
+
+
+	test('transistores([Verde, Azul, Violeta, Naranja, Negro, Gris])', function () {
+		var expected = 567308 ;
+		var result = transistores(['Verde','Azul','Violeta','Naranja','Negro', 'Gris']);
+		expect(result).toBe(expected);
+	});
+
+	test('transistores([Blanco, Verde, Azul, Violeta, Naranja, Negro, Gris])', function () {
+		var expected = 9567308 ;
+		var result = transistores(['Blanco','Verde','Azul','Violeta','Naranja','Negro', 'Gris']);
+		expect(result).toBe(expected);
+	});
